5476612870d4b51334d20295597115b890438b17
wisesumo <samuelbowen@yahoo.com>
2015-07-07 13:56:27 -0400

    uploading sqlite challenge

--- database.py
+++ database.py
@@ -10,26 +10,34 @@ weather = (('Las Vegas', 2013, 'July', 'December'),
                      ('Atlanta', 2013, 'July', 'January'))
 
 
-# connect to the database. 
+#Connect to the database. 
 con = lite.connect('getting_started.db')
 
-# Inserting rows by passing tuples to `execute()`
+# Drop the tables, create tables and insert rows by passing tuples to `execute()`
 with con:
 
     cur = con.cursor()
-    cur.executemany("DROP TABLE IF EXISTS cities;")
-    cur.executemany("DROP TABLE IF EXISTS weather;")
-
-    cur.executemany("CREATE TABLE cities (name text, state text);")
-    cur.executemany("CREATE TABLE weather (city text, year integer, warm_month text, cold_month text, average_high integer);")
+    cur.execute("DROP TABLE IF EXISTS cities;")
+    cur.execute("DROP TABLE IF EXISTS weather;")
+    cur.execute("CREATE TABLE cities (name text, state text);")
+    cur.execute("CREATE TABLE weather (city text, year integer, warm_month text, cold_month text, average_high integer);")
     
     cur.executemany("INSERT INTO cities VALUES(?,?)", cities)
     cur.executemany("INSERT INTO weather VALUES(?,?,?,?)", weather)
 
+# join data in table and load into a pandas DataFrame 
+with con:
 
-    cur.executemany("SELECT name, state, year, warm_month, cold_month FROM cities INNER JOIN weather ON name = city;")
-    
-    input_dataframe = pd.read_csv('getting_started.db')
+    cur = con.cursor()
+    cur.execute("SELECT name, state FROM cities INNER JOIN weather ON name=city WHERE warm_month=?", (month,))
 
-   cur.executemany("")
-   "The cities that are warmest in July are:"
+    rows = cur.fetchall()
+    df = pd.DataFrame(rows)
+
+    print "The cities that are warmest in July are: "
+
+newlist = []
+for i in df.index:
+    newlist.append(df.ix[i,0]+','+df.ix[i,1])
+
+print ", ".join(newlist )
